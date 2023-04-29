dc2a765739d6551c972c0f95cdb2687b5d2ddda6
LMoonsea <ladymoonsea@gmail.com>
2023-04-28 21:35:42 -0300

    login e logout

--- fb.js
+++ fb.js
@@ -1,13 +1,15 @@
 /**
- * Integração com o Firebase.com
+ * Integração com o Firebase
  * By Moonsea
  * MIT License
  **/
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+/**
+ * Configurações so Firebase
+ * 
+ * IMPORTANTE!
+ * Troque os valores de 'firebaseConfig' pelos dados do SEU FIREBASE!
+ **/
 const firebaseConfig = {
   apiKey: "AIzaSyCyA1T8EZs3ZDQJf3aH2B_Sk80nVYZEkGI",
   authDomain: "frontendeirosbiscoiteiros.firebaseapp.com",
@@ -17,68 +19,11 @@ const firebaseConfig = {
   appId: "1:869371915548:web:d6f05042264fe91a70ded9"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Incializa o Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Importa o "core" do Firebase.
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+// Incializa o Firebase Authentication
+const auth = firebase.auth();
 
-// Importa o Authentication do Firebase.
-import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signInWithRedirect,  } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
-
-// Initializa o Firebase.
-const fbapp = initializeApp(firebaseConfig);
-
-// Especifica o provedor de autenticação.
-const provider = new GoogleAuthProvider();
-
-// Inicializa o mecanismo de autenticação.
-const auth = getAuth();
-
-// signInWithPopup(auth, provider)
-
-
-// Observa o status de autenticação do usuário.
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        sessionStorage.userData = JSON.stringify({
-            name: user.displayName,
-            email: user.email,
-            photo: user.photoURL,
-            uid: user.uid,
-            created: user.metadata.createdAt,
-            lastLogin: user.metadata.lastLoginAt 
-        })
-    } else {
-        delete sessionStorage.userData
-    }
-});
-// Executa a jQuery quando o documento estiver pronto
-$(document).ready(myFirebase)
-
-
-function myFirebase() {
-
-    // Detecta cliques no botão de login.
-    $('#navUser').click(login)
-}
-
-function login() {
-// Se o usuario não está logado faz login usando popup.
-    if (!sessionStorage.userData) {
-        signInWithPopup(auth, provider)
-        
-        // Se logou corretamente.    
-        .then(() => {
-            
-            // Redireciona para a 'home'.    
-            location.href = '/home'
-            })
-            // Se está logado...
-    } else
-        
-    // Redireciona para 'profile.
-    location.href = '/profile'
-}
-
+// Define o provedor de autenticação
+var provider = new firebase.auth.GoogleAuthProvider();
